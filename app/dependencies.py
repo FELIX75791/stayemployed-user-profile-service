33621cea5a45571da8af1c2c04e50b8d69742c1f
Ziyue Jin <ziyuejin877@gmail.com>
2024-09-27 23:31:19 -0400

    fix: fixed a minor oauth bug.

--- app/dependencies.py
+++ app/dependencies.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm import Session
 from .services.auth_service import decode_access_token
 from .services.user_service import get_user_by_email
 from .db import get_db
+from .services.auth_service import oauth2_scheme
 
 
-# Extract the current user from the JWT token
-def get_current_user(token: str, db: Session = Depends(get_db)):
+# Use Depends(oauth2_scheme) to inject the token into the function
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     try:
         payload = decode_access_token(token)
         email: str = payload.get("sub")
